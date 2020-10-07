128a825425d4113a10e08138f940d247fbee1799
Zhenhua yu <zyu0628@gmail.com>
2020-10-07 19:44:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(string) {
   return string.toUpperCase()
 }
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhispter(string) {
+  console.log(string.toLowerCase())
+}
