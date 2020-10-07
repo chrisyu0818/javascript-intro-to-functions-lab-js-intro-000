bbfcb582bc095bb1626b627e6e801ae558420cbe
Zhenhua yu <zyu0628@gmail.com>
2020-10-07 19:50:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if (string.toUpperCase())=== string)
+  return "I can't hear you!"
+  if (string.toLowerCase()===string)
+  return "YES INDEED"
+  if (string === "I love you, Grandma")
+  return "I love you, too"
+}
